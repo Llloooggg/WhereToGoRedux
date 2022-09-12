8235271fd17d4b7e197cffadebd1527a96edf19b
Llloooggg <poddeo3@gmail.com>
2022-09-13 01:01:03 +0300

    [frontend}: Добавлена базовая карта

--- frontend/wheretogo/src/components/MapPage.vue
+++ frontend/wheretogo/src/components/MapPage.vue
@@ -1,12 +1,32 @@
 <template>
-  <div class="container">
-    <header class="jumbotron">
-      <h3>Тест</h3>
-    </header>
-  </div>
+  <l-map style="height: 300px" :zoom="zoom" :center="center">
+    <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+    <l-marker :lat-lng="markerLatLng"></l-marker>
+  </l-map>
 </template>
+
+
 <script>
+
+import "leaflet/dist/leaflet.css"
+import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
+
 export default {
-  name: "MapPAge",
+  name: "MapPage",
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+  },
+  data() {
+    return {
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      attribution:
+        '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 15,
+      center: [51.505, -0.159],
+      markerLatLng: [51.504, -0.159],
+    };
+  },
 };
 </script>
